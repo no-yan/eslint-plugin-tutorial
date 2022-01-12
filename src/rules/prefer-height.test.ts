f80a111501bda3208b02b2f33547b2c9cd677a2a
noyan <63000297+no-yan@users.noreply.github.com>
2022-01-13 03:38:58 +0900

    add failing test

--- src/rules/prefer-height.test.ts
+++ src/rules/prefer-height.test.ts
@@ -10,5 +10,11 @@ tester.run("prefer-height", rule, {
     { code: "(props: Props) =><Box height='1'>hello</Box>" },
     { code: "(props: Props) =><Button height={1}>hello</Button>" },
   ],
-  invalid: [{ code: "(props: Props) =><Box h='1'/>", errors: [{ message: "Don't use 'h', use instead 'height'" }] }],
+  invalid: [
+    {
+      code: "(props: Props) =><Box h='1'/>",
+      errors: [{ message: "Don't use 'h', use instead 'height'" }],
+      output: "(props: Props) =><Box height='1'/>",
+    },
+  ],
 });
